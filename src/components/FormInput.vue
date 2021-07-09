64555e1a5f51b5771ae0ac1ace19fc53e176e789
MinsterRobin <minster.robin.pro@gmail.com>
2021-07-09 11:31:08 +0200

    Form Input V1

---
+++ src/components/FormInput.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="form-input-layout">
+        <label v-bind:for="id">{{label}}</label>
+        <input
+                v-bind:type="type"
+                v-bind:pattern="pattern"
+                v-bind:name="id"
+                v-bind:id="id"
+                v-bind:placeholder="placeholder"
+                required
+        >
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FormInput",
+        props: ['label', 'type', 'id', 'placeholder', 'pattern']
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "../styles/breakpoints";
+    @import "../styles/variables";
+
+    .form-input-layout {
+        display: flex;
+        flex-direction: column;
+
+        font-family: $font-family-primary;
+        font-size: $font-size-very-small;
+        font-weight: 600;
+    }
+
+    label {
+        font-family: $font-family-primary;
+        font-size: $font-size-very-small;
+        color:$color-gray-2;
+    }
+
+    input {
+        font-family: $font-family-primary;
+        font-weight: 600;
+        font-size: $font-size-regular;
+        line-height: 20px;
+
+        height: 56px;
+        border: 1px solid #828282;
+        border-radius: 12px;
+    }
+
+</style>
--- src/pages/Checkout/Index.vue
+++ src/pages/Checkout/Index.vue
@@ -1,14 +1,68 @@
 <template>
     <div class="page-layout">
         <PageTitle title="Checkout"/>
+        <form class="form-layout" v-on:submit="onSubmit">
+
+            <FormInput
+                    label="E-mail"
+                    type="email" id="email"
+                    placeholder="Enter your email..."
+            />
+
+            <FormInput
+                    label="Phone"
+                    type="tel" id="phone"
+                    placeholder="Enter your phone..."
+            />
+
+            <FormInput
+                    label="Full name"
+                    type="text" id="fullname"
+                    placeholder="Your full name.."
+            />
+
+            <FormInput
+                    label="Address"
+                    type="text" id="address"
+                    placeholder="Your address.."
+            />
+
+            <FormInput
+                    label="City"
+                    type="text" id="city"
+                    placeholder="Your city.."
+            />
+
+            <label for="country">Country</label>
+            <select name="country" id="country" required>
+                <option>France</option>
+                <option>Germany</option>
+                <option>Italy</option>
+            </select>
+
+            <FormInput
+                    label="Postal code"
+                    type="text" id="postal"
+                    pattern="[0-9]{5}"
+                    placeholder="Your postal code.."
+            />
+
+            <input type="submit" value="Continue">
+        </form>
     </div>
 </template>
 
 <script>
     import PageTitle from "@/components/PageTitle";
+    import FormInput from "@/components/FormInput";
     export default {
         name: "Index",
-        components: {PageTitle}
+        components: {FormInput, PageTitle},
+        methods: {
+            onSubmit : function () {
+                alert("OK");
+            }
+        }
     }
 </script>
 
@@ -31,5 +85,11 @@
         @include breakpoint('s') {
             padding: $padding-small;
         }
+    }
+
+    .form-layout {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
     }
 </style>
